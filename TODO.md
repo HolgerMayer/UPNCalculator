fce170f68f73efeb17f26621a4e39253d5a1cb56
Holger Mayer <holger.mayer@ewetel.net>
2019-12-06 18:11:42 +0100

    [06.12.19 ] [06.12.19] Create Calculator UI Tests

--- TODO.md
+++ TODO.md
@@ -7,12 +7,12 @@
 *[06.12.19]  [06.12.19] Create Calculator UI
 [*06.12.19] [06.12.19] Create Calculator UI Calculation tests
 [06.12.19] [06.12.19] Create Calculator Viewcontroller-Model-Connection
-[06.12.19 ] Create Calculator UI Tests
+[06.12.19 ] [06.12.19] Create Calculator UI Tests
 *[06.12.19] Milestone Git Version
 [06.12.19] Create Jenkins Project
 [06.12.19] Setup XCode Connection in Jenkins
 [06.12.19] Setup Gitub webHook 
-[06.12.19] Configure Unit Tests and Coverage Reporting in jJenkins
+[06.12.19] Configure Unit Tests and Coverage Reporting in Jenkins
 [06.12.19] Milestone Jenkins successfull Integration
 [06.12.19] Setup Fastlane
 
--- UPNCalculatorUITests/UPNCalculatorUITests.swift
+++ UPNCalculatorUITests/UPNCalculatorUITests.swift
@@ -22,16 +22,107 @@ class UPNCalculatorUITests: XCTestCase {
     override func tearDown() {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
     }
-
-    func testExample() {
+    
+    func testAdd1Plus2Equals3() {
         // UI tests must launch the application that they test.
         let app = XCUIApplication()
         app.launch()
 
         // Use recording to get started writing UI tests.
         // Use XCTAssert and related functions to verify your tests produce the correct results.
-    }
+        
+         
+        app.buttons["digit1"].tap()
+        app.buttons["dot"].tap()
+        app.buttons["digit4"].tap()
+        app.buttons["enter"].tap()
+        app.buttons["digit2"].tap()
+        app.buttons["add"].tap()
 
+        let labelText = app.staticTexts.element(matching:.any,identifier: "display").label
+        XCTAssertEqual(labelText, "3.4")
+        
+    }
+    
+    
+    func testSubtract2From1EqualsMinus1() {
+        // UI tests must launch the application that they test.
+        let app = XCUIApplication()
+        app.launch()
+
+        // Use recording to get started writing UI tests.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+        
+         
+        app.buttons["digit1"].tap()
+        app.buttons["enter"].tap()
+        app.buttons["digit2"].tap()
+        app.buttons["subtract"].tap()
+
+        let labelText = app.staticTexts.element(matching:.any,identifier: "display").label
+        XCTAssertEqual(labelText, "-1.0")
+        
+    }
+    
+    
+    func testMultiply2by3equals6() {
+           // UI tests must launch the application that they test.
+           let app = XCUIApplication()
+           app.launch()
+
+           // Use recording to get started writing UI tests.
+           // Use XCTAssert and related functions to verify your tests produce the correct results.
+           
+            
+           app.buttons["digit2"].tap()
+           app.buttons["enter"].tap()
+           app.buttons["digit3"].tap()
+           app.buttons["multiply"].tap()
+
+           let labelText = app.staticTexts.element(matching:.any,identifier: "display").label
+           XCTAssertEqual(labelText, "6.0")
+           
+       }
+
+    func testDivide6by3equals2() {
+             // UI tests must launch the application that they test.
+             let app = XCUIApplication()
+             app.launch()
+
+             // Use recording to get started writing UI tests.
+             // Use XCTAssert and related functions to verify your tests produce the correct results.
+             
+              
+             app.buttons["digit6"].tap()
+             app.buttons["enter"].tap()
+             app.buttons["digit3"].tap()
+             app.buttons["divide"].tap()
+
+             let labelText = app.staticTexts.element(matching:.any,identifier: "display").label
+             XCTAssertEqual(labelText, "2.0")
+             
+         }
+    
+    func testClear() {
+             // UI tests must launch the application that they test.
+             let app = XCUIApplication()
+             app.launch()
+
+             // Use recording to get started writing UI tests.
+             // Use XCTAssert and related functions to verify your tests produce the correct results.
+             
+              
+             app.buttons["digit6"].tap()
+             app.buttons["enter"].tap()
+             app.buttons["digit3"].tap()
+             app.buttons["clear"].tap()
+
+             let labelText = app.staticTexts.element(matching:.any,identifier: "display").label
+             XCTAssertEqual(labelText, "")
+             
+         }
+   
+    /*
     func testLaunchPerformance() {
         if #available(macOS 10.15, iOS 13.0, tvOS 13.0, *) {
             // This measures how long it takes to launch your application.
@@ -40,4 +131,5 @@ class UPNCalculatorUITests: XCTestCase {
             }
         }
     }
+ */
 }

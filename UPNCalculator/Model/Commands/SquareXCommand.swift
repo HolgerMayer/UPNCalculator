fc86e73f2a89e357912d35ac8bfb42200eb2afeb
Holger Mayer <holger.mayer@ewetel.net>
2019-12-15 02:26:58 +0100

    Feature Freeze Functions and layout Unit test erros in stack handling

---
+++ UPNCalculator/Model/Commands/SquareXCommand.swift
@@ -0,0 +1,26 @@
+//
+//  SquareXCommand.swift
+//  UPNCalculator
+//
+//  Created by holgermayer on 15.12.19.
+//  Copyright © 2019 holgermayer. All rights reserved.
+//
+
+import Foundation
+
+
+class SquareXCommand : CalculationCommand {
+    
+    override init(calculatorEngine: UPNEngine, display: Display) {
+        super.init(calculatorEngine: calculatorEngine, display: display)
+    }
+    
+    override  func callEngineCalculation() throws {
+       guard let a =  super.calculatorEngine.top else {
+                display.setError("Please enter x value")
+                return
+            }
+            super.calculatorEngine.enterNumber(a)
+            super.calculatorEngine.multiply()
+    }
+}
